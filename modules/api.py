d35f0a774ac6741e1fe2a3ef299251e114486dfa
Michał Księżuk <michal@ksiezuk.pl>
2020-11-01 00:39:35 +0100

    Refactoring

---
+++ modules/api.py
@@ -0,0 +1,20 @@
+import asyncio
+
+import aiohttp
+
+
+class Api:
+    def __init__(self, hostname, port, protocol):
+        self._endpoint = f"{protocol}://{hostname}:{port}"
+        self._loop = asyncio.get_event_loop()
+
+    async def _async_get(self):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(self._endpoint) as response:
+                    return await response.json()
+            except aiohttp.ClientConnectorError as error:
+                print('Connection Error', str(error))
+
+    def get(self):
+        return self._loop.run_until_complete(self._async_get())
---
+++ modules/utils.py
@@ -0,0 +1,16 @@
+def format_load(values):
+    load_current = round(values["val"], 1)
+    load_min = round(values["min"], 1)
+    load_max = round(values["max"], 1)
+    return f"{load_current} / {load_min} / {load_max}"
+
+
+def format_name(text):
+    words = text.split()
+    brand = words.pop(0)
+    model = " ".join(words)
+    return f"[b]{brand}[/b]\n{model}"
+
+
+def format_temp(value):
+    return f"{str(round(value))}\u00b0"
